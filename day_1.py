f378c33dd6ade77bc4760a7e00be9445db3ac173
J-M Plourde <jmichel.plourde@gmail.com>
2021-12-01 23:31:51 -0500

    Day 1 overkill submission

---
+++ day_1.py
@@ -0,0 +1,37 @@
+import itertools
+
+with open("day1.input") as f:
+    lines = f.readlines()
+    lines = [int(line.rstrip()) for line in lines]
+
+
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def evaluate_increases(groups):
+    increases = 0
+    sums = [sum(i) for i in groups]
+    for i in range(len(sums) - 1):
+        if sums[i + 1] > sums[i]:
+            increases = increases + 1
+
+    print(increases)
+
+
+# Part 1
+increases = 0
+pairs = pairwise(lines)
+evaluate_increases(pairs)
+
+
+def triplewise(iterable):
+    for (a, _), (b, c) in pairwise(pairwise(lines)):
+        yield a, b, c
+
+
+# Part 2
+triplets = triplewise(lines)
+evaluate_increases(triplets)
