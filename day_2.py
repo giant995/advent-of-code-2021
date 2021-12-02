00dcc6d6653aca6c788a580a487dc26925a54fbc
J-M Plourde <jmichel.plourde@gmail.com>
2021-12-02 12:26:12 -0500

    Part 2

--- day_2.py
+++ day_2.py
@@ -1,8 +1,8 @@
-horizontal = 0
-vertical = 0
-
+# Part 1
 with open("day2.input") as f:
-    # part 1
+    horizontal = 0
+    vertical = 0
+
     for line in f:
         coords = line.rstrip().split(" ")
         if coords[0] == "up":
@@ -12,4 +12,22 @@ with open("day2.input") as f:
         elif coords[0] == "forward":
             horizontal += int(coords[1])
 
-print(vertical*horizontal)
+    print(vertical*horizontal)
+
+# Part 2
+with open("day2.input") as f:
+    horizontal = 0
+    vertical = 0
+    aim = 0
+
+    for line in f:
+        coords = line.rstrip().split(" ")
+        if coords[0] == "up":
+            aim -= int(coords[1])
+        elif coords[0] == "down":
+            aim += int(coords[1])
+        elif coords[0] == "forward":
+            horizontal += int(coords[1])
+            vertical += aim * int(coords[1])
+
+    print(vertical*horizontal)
